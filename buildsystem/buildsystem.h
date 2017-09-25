459750170b334eda34548469416db2263d91c4fd
Hugues Morisset <morisset.hugues@gmail.com>
2017-09-25 23:17:09 +0400

    Update GoBuildSystem implementation
    Summary:
    Fix compilation error because of missing virtual method in GoBuildSystem.
    Corresponding commit: kdevelop@0c05bd55965fc6c0468022c935f140408b99151c
    
    Reviewers: apol
    
    Reviewed By: apol
    
    Subscribers: kdevelop-devel
    
    Differential Revision: https://phabricator.kde.org/D7911

--- buildsystem/buildsystem.h
+++ buildsystem/buildsystem.h
@@ -31,6 +31,7 @@ public:
     KDevelop::Path::List includeDirectories(KDevelop::ProjectBaseItem*) const override;
     KDevelop::Path::List frameworkDirectories(KDevelop::ProjectBaseItem*) const override;
     QHash<QString,QString> defines(KDevelop::ProjectBaseItem*) const override;
+    QString extraArguments(KDevelop::ProjectBaseItem* item) const override;
     KDevelop::ProjectTargetItem* createTarget(const QString& target, KDevelop::ProjectFolderItem *parent) override;
     bool addFilesToTarget(const QList<KDevelop::ProjectFileItem*> &files, KDevelop::ProjectTargetItem *parent) override;
     bool removeTarget(KDevelop::ProjectTargetItem *target) override;
